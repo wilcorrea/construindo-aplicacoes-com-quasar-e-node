b0af5554d6962a4ab01ab10c3e6ef134c4ff796e
William Correa <wilcorrea@gmail.com>
2020-08-26 18:38:08 -0300

    ok, that is it!

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import axios from 'axios'
 
 // get baseURL from context
+// use .env, secrets and environment variables
+// to use the appropriate URL here
 Vue.prototype.$axios = axios.create({
   baseURL: 'http://127.0.0.1:3000'
 })
